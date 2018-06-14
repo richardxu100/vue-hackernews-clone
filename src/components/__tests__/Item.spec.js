4226452efea07d36d89f9d5dabb4552f471daf6b
Richard Xu <richard.xu@roadbotics.com>
2018-06-14 10:43:35 -0400

    Configure testing

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -2,14 +2,11 @@ import { shallowMount } from '@vue/test-utils'
 import Item from '@/components/Item.vue'
 
 describe('Item.vue', () => {
-  
+
   it('should render a URL', () => {
-    const item = {
-      url: 'http://some-url.com'
-    }
     const wrapper = shallowMount(Item, {
       propsData: {
-        item
+        item: { url: 'http://some-url.com' }
       }
     })
     expect(wrapper.text()).toContain(item.url)
