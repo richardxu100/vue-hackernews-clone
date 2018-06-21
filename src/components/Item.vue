208ca54d2f7149f713dde527caed20b37e54aa8a
Richard Xu <richard.xu@roadbotics.com>
2018-06-21 12:34:26 -0400

    Add conditional check for displayItems in ItemList

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,7 +1,14 @@
 <template>
   <li class="news-item">
+    <span class="score">{{ item.score }}</span>
     <a :href="item.url">{{ item.title }}</a>
-    {{ item.by }} {{ item.score }}
+    <span class="host"> ({{ item.url }})</span>
+    <br>
+    <span class="meta">
+      <span class="by">
+        by {{ item.by }}
+      </span>
+    </span>
   </li>
 </template>
 
@@ -10,3 +17,38 @@
     props: ['item']
   }
 </script>
+
+<style>
+.news-item {
+	background-color: #fff;
+	padding: 20px 30px 20px 80px;
+	border-bottom: 1px solid #eee;
+	position: relative;
+	line-height: 20px;
+}
+.news-item .score {
+	color: #f60;
+	font-size: 1.1em;
+	font-weight: 700;
+	position: absolute;
+	top: 50%;
+	left: 0;
+	width: 80px;
+	text-align: center;
+	margin-top: -10px;
+}
+.news-item .meta,
+.news-item .host {
+	font-size: 0.85em;
+	color: #828282;
+}
+.news-item .meta a,
+.news-item .host a {
+	color: #828282;
+	text-decoration: underline;
+}
+.news-item .meta a:hover,
+.news-item .host a:hover {
+	color: #f60;
+}
+</style>
--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <item :item="item" v-for="item in displayItems" :key="item.id" />
-    <router-link v-if="page > 1" :to="`/${type}/${page-1}`">
-      &lt; prev
-    </router-link>
-    <a v-else>&lt; prev</a>
-    <span>{{ page }}/{{ maxPage }}</span>
-    <router-link v-if="page < maxPage" :to="`/${type}/${page+1}`">
-      more &gt;
-    </router-link>
-    <a v-else>more &gt;</a>
+  <div class="item-list-view">
+    <div class="item-list-nav">
+      <router-link v-if="page > 1" :to="`/${type}/${page-1}`">
+        &lt; prev
+      </router-link>
+      <a v-else>&lt; prev</a>
+      <span>{{ page }}/{{ maxPage }}</span>
+      <router-link v-if="page < maxPage" :to="`/${type}/${page+1}`">
+        more &gt;
+      </router-link>
+      <a v-else>more &gt;</a>
+    </div>
+    <div class="item-list">
+      <item :item="item" v-for="item in displayItems" :key="item.id" />
+    </div>
   </div>
 </template>
 
@@ -25,10 +29,13 @@ export default {
   props: ['type'],
   beforeMount() {
     this.loadItems()
+    console.log('loading items')
   },
   computed: {
     displayItems() {
-      return this.$store.getters.displayItems
+      if (this.$store.getters.displayItems) {
+        return this.$store.getters.displayItems
+      }
     },
     page() {
       return Number(this.$route.params.page) || 1
@@ -44,6 +51,7 @@ export default {
       try {
         // dispatches fetchListData action
         await this.fetchListData({type: this.type})
+        console.log('displayItems: ', this.displayItems)
         if (this.page < 0 || this.page > this.maxPage) {
           this.$router.replace(`/${this.type}/1`)
         }
@@ -67,6 +75,23 @@ export default {
 	margin: 30px 0;
 	width: 100%;
 	transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+.item-list-nav {
+	padding: 15px 30px;
+	position: fixed;
+	text-align: center;
+	top: 55px;
+	left: 0;
+	right: 0;
+	z-index: 998;
+	box-shadow: 0 1px 2px rgba(0,0,0,0.1);
+  background-color: #fff;
+}
+.item-list-nav a {
+	margin: 0 1em;
+}
+.item-list-nav .disabled {
+	color: #ccc;
 }
 @media (max-width: 600px) {
 	.item-list {
